39593b35a66cba72ac24d27857ace2878c2fd522
Git123 <714642721@qq.com>
2018-12-28 13:22:23 +0800

    姚运与测试拉取

---
+++ parent327/pinyougou-pojo/src/main/java/entity/PageResult.java
@@ -0,0 +1,35 @@
+package entity;
+import java.io.Serializable;
+import java.util.List;
+/**
+ * 分页结果封装对象
+ * @author Administrator
+ *
+ */
+public class PageResult implements Serializable {
+
+
+	private Long total;//总记录数
+	private List rows;//当前页结果	  影响: 编译受影响 .java .class  运行不受影响
+	public PageResult(Long total, List rows) {
+		super();
+		this.total = total;
+		this.rows = rows;
+	}
+
+	public Long getTotal() {
+		return total;
+	}
+
+	public void setTotal(Long total) {
+		this.total = total;
+	}
+
+	public List getRows() {
+		return rows;
+	}
+
+	public void setRows(List rows) {
+		this.rows = rows;
+	}
+}
---
+++ parent327/pinyougou-pojo/src/main/java/pojogroup/SpecificationVo.java
@@ -0,0 +1,39 @@
+package pojogroup;
+
+import cn.itcast.core.pojo.specification.Specification;
+import cn.itcast.core.pojo.specification.SpecificationOption;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * 包装对象 组合对象
+ * 规格对象
+ * 规格选项结果集对象
+ * 不实现序列化接口行不行?   不行   抛异常 SSM 小练习
+ */
+public class SpecificationVo implements Serializable{
+
+
+    //规格对象
+    private Specification specification;
+
+    //规格选项结果集对象
+    private List<SpecificationOption> specificationOptionList;
+
+    public Specification getSpecification() {
+        return specification;
+    }
+
+    public void setSpecification(Specification specification) {
+        this.specification = specification;
+    }
+
+    public List<SpecificationOption> getSpecificationOptionList() {
+        return specificationOptionList;
+    }
+
+    public void setSpecificationOptionList(List<SpecificationOption> specificationOptionList) {
+        this.specificationOptionList = specificationOptionList;
+    }
+}
